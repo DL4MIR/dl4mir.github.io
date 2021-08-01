07c57915ce7ceb84997b68d84f24955a3c1edbd7
iranroman <iran@ccrma.stanford.edu>
2021-08-01 18:24:18 -0400

    adding materials for Day 1

---
+++ md2html.py
@@ -0,0 +1,14 @@
+import markdown
+import sys
+
+filename = sys.argv[1]
+
+header = """| [Home](https://fl4mir.github.io) |\n""" 
+
+with open(filename+'.md', 'r') as f:
+    text = f.read()
+    html = markdown.markdown(header+text)
+
+with open(filename+'.html', 'w') as f:
+    f.write('<body style="font-family: sans-serif">\n')
+    f.write(html)
